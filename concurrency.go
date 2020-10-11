cff3a762383557becc16eebe17f994f03ffc67c0
muzea <mr.muzea@gmail.com>
2020-10-12 02:14:32 +0800

    feat: mvp

---
+++ concurrency.go
@@ -0,0 +1,48 @@
+package main
+
+func dispatch(concurrencyLimit, total int, loop chan int, execEnd, done chan bool) {
+	var concurrency = 0
+	var runIndex = 0
+	for total > runIndex && concurrencyLimit > concurrency {
+		loop <- runIndex
+		runIndex++
+		concurrency++
+	}
+	for {
+		<-execEnd
+		if total > runIndex {
+			loop <- runIndex
+			runIndex++
+		} else {
+			concurrency--
+			if concurrency == 0 {
+				done <- true
+				return
+			}
+		}
+	}
+}
+
+func Run(callback func(int), concurrencyLimit, total int) {
+	loop := make(chan int, concurrencyLimit)
+	execEnd := make(chan bool, concurrencyLimit)
+	done := make(chan bool, 1)
+	var exec = func(index int) {
+		callback(index)
+		execEnd <- true
+	}
+	go dispatch(concurrencyLimit, total, loop, execEnd, done)
+	for {
+		select {
+		case index := <-loop:
+			{
+				go exec(index)
+				break
+			}
+		case <-done:
+			{
+				return
+			}
+		}
+	}
+}
---
+++ concurrency_test.go
@@ -0,0 +1,48 @@
+package main
+
+import (
+	"sync/atomic"
+	"testing"
+
+	"github.com/stretchr/testify/assert"
+)
+
+func TestCounter1(t *testing.T) {
+	var count = int32(0)
+	var total = 100
+	Run(func(index int) {
+		atomic.AddInt32(&count, 1)
+	}, 100, total)
+	assert.Equal(t, int(count), total)
+}
+
+func TestCounter2(t *testing.T) {
+	var count = int32(0)
+	var total = 1000
+
+	Run(func(index int) {
+		atomic.AddInt32(&count, 1)
+	}, 100, total)
+	assert.Equal(t, int(count), total)
+}
+
+func BenchmarkConcurrency1Loop10(b *testing.B) {
+	for n := 0; n < b.N; n++ {
+		Run(func(index int) {}, 1, 10)
+	}
+}
+
+func BenchmarkNormalLoop10(b *testing.B) {
+	for n := 0; n < b.N; n++ {
+		var cb = func(index int) {}
+		for i := 0; i < 10; i++ {
+			cb(i)
+		}
+	}
+}
+
+func BenchmarkConcurrency1000Loop10000(b *testing.B) {
+	for n := 0; n < b.N; n++ {
+		Run(func(index int) {}, 1000, 10000)
+	}
+}
